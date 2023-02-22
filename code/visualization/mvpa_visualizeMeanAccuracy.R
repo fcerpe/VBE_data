44547dff01a9f7ca2029c128f78f372b7ca564c9
Filippo Cerpelloni <filippo.cerpelloni@gmail.com>
2023-02-22 11:25:30 +0100

    mvpa and mean decoding visualization

--- code/visualization/mvpa_visualizeMeanAccuracy.R
+++ code/visualization/mvpa_visualizeMeanAccuracy.R
@@ -3,12 +3,10 @@ library("tidyverse")
 library("reshape2")
 library("gridExtra")
 
-decodingMeans <- read_excel("/Users/cerpelloni/Desktop/GitHub/VisualBraille_data/code/mvpa/comparison_experts_means_modified.xlsx")
+decodingMeans <- read_excel("/Users/cerpelloni/Desktop/GitHub/VisualBraille_data/code/mvpa/comparison_experts_means.xlsx")
 
 # Modify the data to get a color-coding column, to split between images
 df_means <- as.data.frame(decodingMeans)
-
-df_means$scriptColor = df_means$decodingCondition
 
 # divide different images: two plots
 means_div <- group_split(df_means, image)
@@ -17,27 +15,31 @@ means_div <- group_split(df_means, image)
 means_beta <- means_div[[1]]
 means_tmap <- means_div[[2]]
 
-means_beta = unite(means_beta, ids, c(area, decodingCondition))
 means_beta = mutate(means_beta, image = NULL)
-means_tmap = unite(means_tmap, ids, c(area, decodingCondition))
 means_tmap = mutate(means_tmap, image = NULL)
 
-plotBeta <- ggplot(means_beta, aes(x = ids, y = accuracy))
-plotBeta + geom_boxplot(outlier.shape = NA, aes(colour = scriptColor)) + 
+plotBeta <- ggplot(means_beta, aes(x = decodingCondition, y = accuracy), middle = mean(accuracy))
+plotBeta + geom_boxplot(outlier.shape = NA, aes(colour = script)) + 
   theme_classic() + 
   geom_hline(aes(yintercept = 0.5), size = .25, linetype = "dashed") +  
   ylim(0,1) +
-  geom_jitter(aes(colour = scriptColor), width = 0.2) +
-  facet_grid(cols = vars(nbVoxels)) + 
-  labs(x = "Area", y = "Accuracy", title = "Mean decoding acccuracy - beta")
+  geom_jitter(aes(colour = script), width = 0.3) +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + 
+  facet_grid(. ~ nbVoxels + factor(area, levels = c("VWFAfr", "lLO", "rLO")), switch = "x", labeller = label_value) + 
+  labs(x = "nbVoxels x Area", y = "Accuracy", title = "Mean decoding acccuracy - beta")
 
-plotTmap <- ggplot(means_tmap, aes(x = ids, y = accuracy))
-plotTmap + geom_boxplot(outlier.shape = NA, aes(colour = scriptColor)) + 
+ggsave("mean_accuracy_beta.png", width = 2600, height = 1600, dpi = 320, units = "px")
+
+
+plotTmap <- ggplot(means_tmap, aes(x = decodingCondition, y = accuracy))
+plotTmap + geom_boxplot(outlier.shape = NA, aes(colour = script)) + 
   theme_classic() + 
   geom_hline(aes(yintercept = 0.5), size = .25, linetype = "dashed") +  
   ylim(0,1) +
-  geom_jitter(aes(colour = scriptColor), width = 0.2) +
-  facet_grid(cols = vars(nbVoxels)) + 
-  labs(x = "Area", y = "Accuracy", title = "Mean decoding acccuracy - tmap")
+  geom_jitter(aes(colour = script), width = 0.3) +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank()) + 
+  facet_grid(. ~ nbVoxels + factor(area, levels = c("VWFAfr", "lLO", "rLO")), switch = "x", labeller = label_value) + 
+  labs(x = "nbVoxels x Area", y = "Accuracy", title = "Mean decoding acccuracy - tmap")
 
+ggsave("mean_accuracy_tmap.png", width = 2600, height = 1600, dpi = 320, units = "px")
 
